e4219572d183975864c5c16b8c963ed7a5244c8f
Sp4mm3rs <pt.juliocesar@gmail.com>
2018-06-20 19:16:42 -0500

    actualizando

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+
+txtBase = document.getElementById("txt_base");
+
 $(document).ready(function(){
 
     $(".views-row").each(function(i){
 
     	$(this).addClass("item" + (i+1));
-
 	       $(this).click(function(){
 	       		$('section#form').show();
 	       		$('#hTitle').append($(this).find('h2').text());
