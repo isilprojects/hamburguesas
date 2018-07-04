37d67f1b4f2f7d52bcf658a3d82c3b0a2924f342
Julio Pillaca <pt.juliocesar@gmail.com>
2018-07-04 13:12:39 -0500

    Modificando valores del producto

--- js/main.js
+++ js/main.js
@@ -12,51 +12,33 @@ $(document).ready(function(){
         $('section#form').hide();
         $('#hTitle').empty();
         $('#pTotal').empty();
-        $("#ad:checkbox").attr('checked', false);
+        // $("#ad:checkbox").attr('checked', false);
     });    
 });
 
+
+var productos = document.getElementById("itemPro");
+// console.log (productos);
+
+for (var i = 0; i < productos.length; i++) {
+    var vista = productos[i];
+    console.log(vista);
+}
+
+
 function calcularPrecio(){
     obj = document.miform['ad'];
+    console.log(obj);
     totalChecks = obj.length;
     totalSumado = 0;
     for( i=0; i<totalChecks; i++){ 
-        switch(i) {
-            case 1:
-                pProducto = 9.90;
-                break;
-            case 2:
-                pProducto = 10.90;
-                break;
-            case 3:
-                pProducto = 9.90;
-                break;
-            case 4:
-                pProducto = 11.90;
-                break;
-            case 5:
-                pProducto = 9.90;
-                break;
-            case 6:
-                pProducto = 12.90;
-                break;
-            case 7:
-                pProducto = 14.90;
-                break;
-            case 8:
-                pProducto = 16.90;
-                break;
-            default:
-                0
-        }
         if( obj[i].checked == true ){
             valor = obj[i].value;        
             totalSumado = totalSumado + Number(valor);
         }
     }
-    document.getElementById('pTotal').innerHTML = totalSumado + pProducto; 
+    document.getElementById('pTotal').innerHTML = totalSumado; 
 }
-
 
 
 
